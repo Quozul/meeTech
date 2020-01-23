f604caf6de7344c76de05141a0f1955ed275af39
SarahSch19 <60216534+SarahSch19@users.noreply.github.com>
2020-01-23 23:27:42 +0100

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -5,5 +5,5 @@
     <meta name="description" content="Hardware blog, forum and PC configurator.">
     <meta name="author" content="">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style/main.css"
+    <link rel="stylesheet" href="./style/main.css">
 </head>
