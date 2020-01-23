951c578485c23054f66edf60baca33c1d26641c4
Quozul <quozul@outlook.com>
2020-01-23 11:10:59 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# meeTech
+
+Hardware configuration website to find the perfect computer according to your needs.
---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <title>meeTech - Hardware website</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a>Accueil</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h1>WORK IN PROGRESS</h1>
+    </main>
+
+    <footer>Copyright</footer>
+</body>
+
+</html>
