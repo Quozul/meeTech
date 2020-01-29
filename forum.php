8e3ad9df091e3e1a534b7ec24556391a0165d07f
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-01-29 18:58:44 +0100

    Forum

---
+++ forum.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<?php include('includes/head.php') ; ?>
+<body>
+	<?php include('includes/header.php') ; ?>
+
+	<main role="main" class="container">
+		<section class="jumbotron">
+			<h1>Forum</h1>
+			<article></article>
+		</section>
+	</main>
+
+	<?php include('includes/footer.php') ; ?>
+</body>
+</html>
