fb7dcabf3f16523bf62b7ff2ba45a27a3f6b47f6
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-01-29 18:38:03 +0100

    Blog update

--- blog.php
+++ blog.php
@@ -8,27 +8,28 @@
 	<?php include('includes/header.php') ; ?>
 
 	<main role="main" class="container">
-		<section class="jumbotron">
-			<article>
-				<h1>Titre de l'article 1</h1>
-				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<aside><img src="/images/logo.svg" alt="logo du site"></aside>
-			</article>
+		<article class="jumbotron">
+			<h1>Titre de l'article 1</h1>
+			<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<aside><img src="/images/logov2.svg" alt="logo du site"></aside>
+			<button type="button" class="btn btn-primary">Lire la suite</button>
+		</article>
 
-			<article>
-				<h1>Titre de l'article 2</h1>
-				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<aside><img src="/images/logo.svg" alt="logo du site"></aside>
-			</article>
+		<article class="jumbotron">
+			<h1>Titre de l'article 2</h1>
+			<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<aside><img src="/images/logov2.svg" alt="logo du site"></aside>
+			<button type="button" class="btn btn-primary">Lire la suite</button>
+		</article>
 
-			<article>
-				<h1>Titre de l'article 3</h1>
-				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<aside><img src="/images/logo.svg" alt="logo du site"></aside>
-			</article>
-		</section>
+		<article class="jumbotron">
+			<h1>Titre de l'article 3</h1>
+			<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<aside><img src="/images/logov2.svg" alt="logo du site"></aside>
+			<button type="button" class="btn btn-primary">Lire la suite</button>
+		</article>
 	</main>
 
-	<?php include('includes/header.php') ; ?>
+	<?php include('includes/footer.php') ; ?>
 </body>
 </html>
