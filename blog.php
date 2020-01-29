a9708532c9c58c90190f30f18e5a5fa42891250c
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-01-29 18:20:42 +0100

    Blog test

---
+++ blog.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<?php
+	$head = "Blog" ;
+	include('/includes/head.php') ;
+?>
+<body>
+	<?php include('/includes/header.php') ; ?>
+
+	<main role="main" class="container">
+		<section class="jumbotron">
+			<article>
+				<h1>Titre de l'article 1</h1>
+				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<aside><img src="/images/logo.svg" alt="logo du site"></aside>
+			</article>
+
+			<article>
+				<h1>Titre de l'article 2</h1>
+				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<aside><img src="/images/logo.svg" alt="logo du site"></aside>
+			</article>
+
+			<article>
+				<h1>Titre de l'article 3</h1>
+				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+				<aside><img src="/images/logo.svg" alt="logo du site"></aside>
+			</article>
+		</section>
+	</main>
+
+	<?php include('/includes/header.php') ; ?>
+</body>
+</html>
