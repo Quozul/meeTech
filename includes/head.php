86a219ea67a83e1f212ca4f0f10309cfebe317e7
Quozul <erwan.legloannec@dartybox.com>
2020-01-30 18:33:39 +0100

    Updated logo

--- includes/head.php
+++ includes/head.php
@@ -13,5 +13,5 @@
     <meta name="author" content="">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="/style/main.css">
-    <link rel="icon" type="image/png" href="/assets/logov2.png">
+    <link rel="icon" type="image/png" href="/assets/logov4.png">
 </head>
