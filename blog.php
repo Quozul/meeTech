521337117df148ded07888d173b3eb603fac1144
Quozul <quozul@outlook.com>
2020-01-30 11:40:45 +0100

    Updated blog

--- blog.php
+++ blog.php
@@ -1,35 +1,40 @@
 <!DOCTYPE html>
 <html>
 <?php
-	$head = "Blog" ;
-	include('includes/head.php') ;
+$head = "Blog";
+include('includes/head.php');
 ?>
+
 <body>
-	<?php include('includes/header.php') ; ?>
+	<?php include('includes/header.php'); ?>
 
 	<main role="main" class="container">
-		<article class="jumbotron">
-			<h1>Titre de l'article 1</h1>
-			<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<aside><img src="/images/logov2.svg" alt="logo du site"></aside>
-			<button type="button" class="btn btn-primary">Lire la suite</button>
+		<article class="jumbotron d-flex justify-content-center align-items-stretch flex-row">
+			<span>
+				<h1><a href="/message/?id=1">Title of article 1</a></h1>
+				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <a>Continue reading &raquo;</a></p>
+			</span>
+			<aside><img src="/images/logov2.svg" class="rounded w-50" alt="logo du site"></aside>
 		</article>
 
-		<article class="jumbotron">
-			<h1>Titre de l'article 2</h1>
-			<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<aside><img src="/images/logov2.svg" alt="logo du site"></aside>
-			<button type="button" class="btn btn-primary">Lire la suite</button>
+		<article class="jumbotron d-flex justify-content-center align-items-stretch flex-row">
+			<span>
+				<h1><a href="/message/?id=1">Title of article 2</a></h1>
+				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <a>Continue reading &raquo;</a></p>
+			</span>
+			<aside><img src="/images/logov2.svg" class="rounded w-50" alt="logo du site"></aside>
 		</article>
 
-		<article class="jumbotron">
-			<h1>Titre de l'article 3</h1>
-			<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<aside><img src="/images/logov2.svg" alt="logo du site"></aside>
-			<button type="button" class="btn btn-primary">Lire la suite</button>
+		<article class="jumbotron d-flex justify-content-center align-items-stretch flex-row">
+			<span>
+				<h1><a href="/message/?id=1">Title of article 2</a></h1>
+				<p>Résumé de l'article, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <a>Continue reading &raquo;</a></p>
+			</span>
+			<aside><img src="/images/logov2.svg" class="rounded w-50" alt="logo du site"></aside>
 		</article>
 	</main>
 
-	<?php include('includes/footer.php') ; ?>
+	<?php include('includes/footer.php'); ?>
 </body>
+
 </html>
