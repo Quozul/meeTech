7677b2b815f18fb7ef5a10c93b6fc815b26aa9ef
RevanSpec <desbrueresa1@gmail.com>
2020-03-08 15:50:11 +0100

    verif

--- verification.php
+++ verification.php
@@ -2,7 +2,7 @@
 <html>
 <?php include('includes/head.php'); ?>
 
-<body>
+<body class="d-flex vh-100 flex-column justify-content-between">
     <?php include('includes/header.php');
     $sth = $pdo->prepare('UPDATE users SET verified = TRUE WHERE token =?');
     $sth->execute([$_GET['token']]);
