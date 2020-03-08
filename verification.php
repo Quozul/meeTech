beb1a4d653517df70f56114b1cb62ae5ee7106e2
Quozul <erwan.legloannec@dartybox.com>
2020-03-08 15:24:41 +0100

    Fixes 2

--- verification.php
+++ verification.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-<?php include('/includes/head.php'); ?>
+<?php include('includes/head.php'); ?>
 
 <body>
-    <?php include('/includes/header.php');
+    <?php include('includes/header.php');
     $sth = $pdo->prepare('UPDATE users SET verified = TRUE WHERE token =?');
     $sth->execute([$_GET['token']]);
     ?>
     <main>
         <p>Votre compte a été validé avec succés</p>
     </main>
-    <?php include('/includes/footer.php'); ?>
+    <?php include('includes/footer.php'); ?>
 </body>
 
 </html>
