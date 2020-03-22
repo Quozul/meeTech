2598f49a5276b3e353ed3ae964a183254c746bd0
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-03-22 12:04:27 +0100

    Redirections

--- actions/languages/drop_language.php
+++ actions/languages/drop_language.php
@@ -1,14 +1,14 @@
 <?php
-require('../../config.php') ; //require($_SERVER['DOCUMENT_ROOT'] . '/config.php') ;
+require($_SERVER['DOCUMENT_ROOT'] . '/config.php') ;
 
 if(!isset($_GET['lang']) || empty($_GET['lang'])) {
-  header('location: /meetech/?error=unset') ; //header('location:' . $_SERVER['DOCUMENT_ROOT'] . '/admin/languages/?error=setlang') ;
+  header('location:' . $_SERVER['DOCUMENT_ROOT'] . '/admin/languages/?error=setlang') ;
   exit() ;
 }
 
 $language = htmlspecialchars($_GET['lang']) ;
 $q = $pdo->prepare('DELETE FROM language WHERE lang = ?') ;
 $q->execute([$language]);
-header('location: /meetech/?success=drop') ; //header('location:' . $_SERVER['DOCUMENT_ROOT'] . '/admin/languages/') ;
+header('location:' . $_SERVER['DOCUMENT_ROOT'] . '/admin/languages/?success=drop') ;
 exit() ;
 ?>
