8246c2ad2acd5533e1e7b98b1a2b059bcb491213
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-03-22 15:27:08 +0100

    Delete language

--- admin/actions/languages/drop_language.php
+++ admin/actions/languages/drop_language.php
@@ -1,11 +1,6 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . '/config.php') ;
 
-if(!isset($_GET['lang']) || empty($_GET['lang'])) {
-  header('location: ../../languages/?error=setlang') ;
-  exit() ;
-}
-
 $language = htmlspecialchars($_GET['lang']) ;
 $q = $pdo->prepare('DELETE FROM language WHERE lang = ?') ;
 $q->execute([$language]);
