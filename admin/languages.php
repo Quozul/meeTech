8ec15d89b1a5d4106a59e3a9c324c26ea55552fb
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-03-22 15:14:25 +0100

    Alertes langue

--- admin/languages.php
+++ admin/languages.php
@@ -43,10 +43,25 @@
                 <hr>
                 <p><a href="https://www.alt-codes.net/flags" target="_blank">Liens unicode des drapeaux</a></p>
                 <?php
+                $message = '' ;
                 if(isset($_GET['error'])) {
                   if(htmlspecialchars($_GET['error']) == 'setlang') $message = 'Saisissez un nom de langue' ;
                   else if (htmlspecialchars($_GET['error']) == 'elsewhere') $message = 'La valeur entrée existe déjà' ;
-                  else $message = '' ;
+                  if (!empty($message)) {
+                ?>
+                <hr>
+                <div class="alert alert-danger" role="alert">
+                  <?php echo $message ; ?>
+                </div>
+                <?php
+                  }
+                }
+
+                $message = '' ;
+                if(isset($_GET['error'])) {
+                  if(htmlspecialchars($_GET['success']) == 'edit') $message = 'Langue éditée' ;
+                  else if (htmlspecialchars($_GET['success']) == 'add') $message = 'Langue ajoutée' ;
+                  else if (htmlspecialchars($_GET['success']) == 'drop') $message = 'Langue supprimée' ;
                   if (!empty($message)) {
                 ?>
                 <hr>
