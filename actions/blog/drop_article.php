7209650f2636c7eef0d50425ab6628e8b1fd48ff
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-04-06 19:24:32 +0200

    Minor drop fixes

--- actions/blog/drop_article.php
+++ actions/blog/drop_article.php
@@ -1,8 +1,15 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT'] . '/config.php') ;
 $article = htmlspecialchars($_GET['post']) ;
+
+$q = $pdo->prepare('SELECT category FROM message WHERE id_m = ?') ;
+$q->execute([$article]) ;
+$category = $q->fetch() ;
+$category = $category['category'] ;
+
 $q = $pdo->prepare('DELETE FROM message WHERE id_m = ?') ;
 $q->execute([$article]) ;
-header('location : /blog/?success=drop') ;
+
+header('location : /' . $category . '/?success=drop') ;
 exit() ;
 ?>
