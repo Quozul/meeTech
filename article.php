92ad1c690c2953948f8e7e7702050b53433592f1
Sarah Schlegel <grepo.sarah19@gmail.com>
2020-04-09 22:01:11 +0200

    Article comments

--- article.php
+++ article.php
@@ -1,165 +1,213 @@
 <!DOCTYPE html>
 <html>
-    <?php
-        include('includes/head.php') ;
-        include('includes/blog/display_c.php') ;
-        include('includes/blog/get_related_c.php') ;
-        include('includes/blog/comment_modal.php') ;
+  <?php
+      include('includes/head.php') ;
 
-        $page_limit = 10 ;
-        $page = isset($_GET['page']) ? $_GET['page'] : 1;
-        $exists = 0 ;
-    ?>
+      $page_limit = 10 ;
+      $page = isset($_GET['page']) ? $_GET['page'] : 1;
+      $exists = 0 ;
+  ?>
 
-    <body class="d-flex vh-100 flex-column justify-content-between">
-        <?php include('includes/header.php') ; ?>
-        <main role="main" class="container">
-            <section class="jumbotron">
+  <body class="d-flex vh-100 flex-column justify-content-between">
+      <?php include('includes/header.php') ; ?>
+      <main role="main" class="container">
+          <section class="jumbotron">
 
 <!-- The $_GET['post'] isn't set -->
-            <?php
-            if (!isset($_GET['post']) || is_null($_GET['post'])) {
-              include('includes/nothing.php')  ;
-            } else {
-              $message_id = strip_tags($_GET['post']) ;
-              $query = $pdo->prepare('SELECT COUNT(id_m) FROM message WHERE id_m = ?') ;
-              $query->execute([$message_id]) ;
-              $exists = $query->fetch()[0] ;
-              if ($exists == 0) {
-              ?>
-
+          <?php
+          if (!isset($_GET['post']) || is_null($_GET['post'])) {
+            include('includes/nothing.php')  ;
+          } else {
+            $message_id = strip_tags($_GET['post']) ;
+            $query = $pdo->prepare('SELECT username, author, title, content, date_published, date_edited,
+              default_language, icon, category, file_name FROM message
+              LEFT JOIN users ON id_u = author
+              LEFT JOIN file ON message = id_m
+              LEFT JOIN language ON lang = default_language
+              WHERE id_m = ?') ;
+            $query->execute([$message_id]) ;
+            $message = $query->fetch() ;
+            if (!isset($message) || empty($message)) {
+            ?>
 <!-- The $_GET['post'] is set but no matching article in DB -->
-              <h1>Whoups, this article doesn't exist !</h1>
-              <hr>
-              <a href="/blog/" type="button" class="btn btn-primary">Go to Blog</a>
-              <a href="/forum/" type="button" class="btn btn-primary">Go to Forum</a>
+            <h1>Whoups, this article doesn't exist !</h1>
+            <hr>
+            <a href="/blog/" type="button" class="btn btn-primary">Go to Blog</a>
+            <a href="/forum/" type="button" class="btn btn-primary">Go to Forum</a>
 
 <!-- Valid article display -->
-              <?php
-              } else {
-                $query = $pdo->prepare('SELECT username, author, title, content, date_published, date_edited,
-                  default_language, icon, category, file_name FROM message
-                  LEFT JOIN users ON id_u = author
-                  LEFT JOIN file ON message = id_m
-                  LEFT JOIN language ON lang = default_language
-                  WHERE id_m = ?') ;
-                $query->execute([$message_id]) ;
-                $message = $query->fetchAll()[0] ;
-                include('includes/blog/edit_modal.php') ;
-              ?>
-              <h1><?= $message['title'] ; ?></h1>
+            <?php
+            } else {
+              include('includes/blog/edit_modal.php') ;
+            ?>
+            <h1><?= $message['title'] ; ?></h1>
 
-              <div class="float-right">
-                <span><?= $message['icon'] ; ?></span>
-                <?php if (isset($_SESSION['userid']) && $_SESSION['userid'] == $message['author']) { ?>
-                <button type="button" class="btn btn-outline-secondary btn-sm" data-toggle="modal" data-target="#editModal">Éditer</button>
-                <?php } ?>
-                <a href="/actions/blog/report_article.php?post=<?= $message_id ; ?>" type="button" class="btn btn-outline-danger btn-sm">Signaler</a>
-              </div>
-
-              <small class="text-muted">
-                Published on <?= $message['date_published'] ?> by
-                <a href="user/?id=<?= $message['author'] ; ?>"><?= $message['username'] ; ?></a>
-              </small>
-              <hr>
-
-              <?php if (!empty($message['file_name'])) { ?>
-              <img src="images/<?= $message['file_name'] ; ?>" class="rounded float-left mb-3 mr-3" alt="Image of article <?= $message_id ; ?>" style="max-width:250px;max-height:250px;">
+            <div class="float-right">
+              <span><?= $message['icon'] ; ?></span>
+              <?php if (isset($_SESSION['userid']) && $_SESSION['userid'] == $message['author']) { ?>
+              <button type="button" class="btn btn-outline-secondary btn-sm" data-toggle="modal" data-target="#editModal">Éditer</button>
               <?php } ?>
+              <a href="/actions/blog/report_article.php?post=<?= $message_id ; ?>" type="button" class="btn btn-outline-danger btn-sm">Signaler</a>
+            </div>
 
-              <div class="markdown"><?= $message['content'] ; ?></div>
+            <small class="text-muted">
+              Published on <?= $message['date_published'] ?> by
+              <a href="user/?id=<?= $message['author'] ; ?>"><?= $message['username'] ; ?></a>
+            </small>
+            <hr>
 
-              <button id="articleMark" type="button" class="btn btn-success" onclick="markArticle()"></button>
+            <?php if (!empty($message['file_name'])) { ?>
+            <img src="images/<?= $message['file_name'] ; ?>" class="rounded float-left mb-3 mr-3" alt="Image of article <?= $message_id ; ?>" style="max-width:250px;max-height:250px;">
+            <?php } ?>
 
-              <script type="text/javascript">
-              let article = <?php echo $message_id ; ?> ;
-              let user = <?php  if (isset($_SESSION['userid'])) echo $_SESSION['userid'] ;
-                                else echo '0' ;
-                         ?> ;
-              const voteButton = document.getElementById('articleMark') ;
-              getArticleMark() ;
+            <div class="markdown"><?= $message['content'] ; ?></div>
 
-              function markArticle() {
-                if (user != 0) {
-                  const request = new XMLHttpRequest() ;
-                  request.open('POST', '/actions/blog/mark_article/') ;
-                  request.onreadystatechange = function() {
-                    if (request.readyState === 4) {//event de fin de requête XMLHttpRequest
-                      const success = parseInt(request.responseText);
-                      if (success === 1) {
-                        voteButton.disabled = 'disabled' ;
-                        getArticleMark() ;
-                      } else if (success === -1) {
-                        alert("Vous devez être connecté pour voter pour un article.") ;
-                      } else if (success === -2) {
-                        alert("Ne jouez pas avec l'Ajax !") ;
-                      } else if (success === -3) {
-                        alert("Vous avez déjà voté pour cet article.") ;
-                      } else {
-                        alert("Une erreur est survenue") ;
-                      }
-                    }
-                  };
-                  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                  request.send(`message_id=${article}`);
-                } else {
-                  alert("Vous devez être connecté pour voter pour un article.") ;
-                }
-              }
+            <button id="articleMark" type="button" class="btn btn-success" onclick="markArticle()"></button>
 
-              function getArticleMark() {
+            <script type="text/javascript">
+            let article = <?php echo $message_id ; ?> ;
+            let user = <?php  if (isset($_SESSION['userid'])) echo $_SESSION['userid'] ;
+                              else echo '0' ;
+                       ?> ;
+            const voteButton = document.getElementById('articleMark') ;
+            getArticleMark() ;
+
+            function markArticle() {
+              if (user != 0) {
                 const request = new XMLHttpRequest() ;
-                request.open('POST', '/includes/blog/get_article_mark/') ;
+                request.open('POST', '../actions/blog/mark_article/') ;
                 request.onreadystatechange = function() {
                   if (request.readyState === 4) {//event de fin de requête XMLHttpRequest
-                    voteButton.innerHTML = '+ ' +  request.responseText ;
+                    const success = parseInt(request.responseText);
+                    if (success === 1) {
+                      voteButton.disabled = 'disabled' ;
+                      getArticleMark() ;
+                    } else if (success === -1) {
+                      alert("Vous devez être connecté pour voter pour un article.") ;
+                    } else if (success === -2) {
+                      alert("Ne jouez pas avec l'Ajax !") ;
+                    } else if (success === -3) {
+                      alert("Vous avez déjà voté pour cet article.") ;
+                    } else {
+                      alert("Une erreur est survenue") ;
+                    }
                   }
                 };
                 request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
                 request.send(`message_id=${article}`);
-              }
-              </script>
-            <?php
-              }
-            }
-            ?>
-            </section>
-
-            <?php
-            if (isset($exists) && $exists != 0 && !is_null($message[0])) {
-            ?>
-<!-- Comments display -->
-            <section class="jumbotron">
-              <?php
-              $query = $pdo->prepare('SELECT COUNT(id_c) FROM comment WHERE parent_message = ? AND parent_comment = ?') ;
-              $query->execute([$message_id, 0]) ;
-              $elements = $query->fetch()[0] ;
-              if ($elements == 0) {
-              ?>
-              <p>Would you like to <a href="#collapseResp0">comment</a> on this ?</p>
-              <?php
               } else {
-                $query = $pdo->prepare('SELECT id_c, author, parent_message, content, date_published, date_edited FROM comment WHERE parent_message = ? AND parent_comment = ?') ;
-                $query->execute([$message_id, 0]) ;
-                $comments = $query->fetchAll() ;
-
-                $offset = $page_limit * ($page - 1) ;
-                for ($i = $offset ; $i < $offset + $page_limit && $i < $elements ; $i++) {
-                    $increment = 1 ;
-                    displayComment($i, $increment, $comments[$i], $pdo) ;
-                }
-                ?>
-                <button type="button" class="btn btn-primary" data-toggle="collapse" href="#collapseResp0" aria-expanded="false" aria-controls="collapseResp">Comment</button>
-            <?php
+                alert("Vous devez être connecté pour voter pour un article.") ;
               }
-            commentModal(0) ;
             }
-            ?>
-            </section>
-        </main>
-        <?php include('includes/footer.php') ; ?>
 
-        <script src="/scripts/markdown.js" charset="utf-8"></script>
-        <script src="/scripts/blogVerifications.js" charset="utf-8"></script>
-    </body>
+            function getArticleMark() {
+              const request = new XMLHttpRequest() ;
+              request.open('POST', '../includes/blog/get_article_mark/') ;
+              request.onreadystatechange = function() {
+                if (request.readyState === 4) {//event de fin de requête XMLHttpRequest
+                  voteButton.innerHTML = '+ ' +  request.responseText ;
+                }
+              };
+              request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+              request.send(`message_id=${article}`);
+            }
+            </script>
+          </section>
+
+<!-- Comments display -->
+          <section class="jumbotron">
+              <?php if (isset($_SESSION['userid']) && !empty($_SESSION['userid'])) { ?>
+                  <form>
+                      <div class="form-group">
+                          <label for="comment" id="comment-label">Commentaire</label>
+                          <textarea class="form-control" id="comment" name="comment" placeholder="Écrivez un commentaire..."></textarea>
+                      </div>
+                      <button type="button" onclick="submit_comment(<?php echo $article; ?>);" class="btn btn-primary">Poster</button>
+                  </form>
+              <?php } else { ?>
+                  <div class="alert alert-info">Vous devez être connecté pour poster un commentaire.</div>
+              <?php } ?>
+              <hr>
+              <div id="comments"><?php include('includes/blog/comments.php') ; ?></div>
+
+              <script>
+                  let answers = null;
+
+                  function update_comments() {
+                      const url_params = new URLSearchParams(window.location.search);
+                      const article = url_params.get('post');
+                      getHtmlContent('../includes/blog/comments.php', `post=${article}`).then((res) => {
+                          document.getElementById('comments').innerHTML = res;
+                      });
+                  }
+
+                  function submit_comment() {
+                      let query = `id=${article}&comment=${document.getElementById('comment').value}`;
+
+                      const answers = comment_get_answer();
+                      if (answers != null)
+                          query += `&answers=${answers}`;
+
+                      request('/actions/hardware/submit_comment.php', query).then((e) => {
+                          console.log(e.resonse);
+
+                          update_comments();
+
+                          document.getElementById('comment').value = '';
+                      });
+                  }
+
+                  function comment_get_answer() {
+                      return answers;
+                  }
+
+                  function aswer_comment(comment_id) {
+                      const comments = document.getElementsByClassName('comment');
+                      const comment = document.getElementById(`comment-${comment_id}`);
+                      const comment_label = document.getElementById('comment-label');
+
+                      if (!comment.classList.contains('bg-light')) {
+
+                          for (const key in comments)
+                              if (comments.hasOwnProperty(key)) {
+                                  const element = comments[key];
+                                  if (element.classList.contains('bg-light'))
+                                      element.classList.remove('bg-light');
+                              }
+
+                          comment.classList.add('bg-light');
+                          answers = comment_id;
+
+                          comment_label.innerHTML = `Répondre à ${comment.getElementsByClassName('comment-author')[0].innerHTML}`;
+                      } else {
+                          comment.classList.remove('bg-light');
+                          comment_label.innerHTML = `Commentaire`;
+                          answers = null;
+                      }
+                  }
+
+                  function remove_comment(comment_id) {
+                      let query = `id=${comment_id}`;
+
+                      request('/actions/hardware/delete_comment.php', query).then((e) => {
+                          console.log(e.resonse);
+                          update_comments();
+                      });
+                  }
+
+                  function page_update_component() {
+                      document.getElementById('close-edit-component-modal').click();
+                      window.location.reload();
+                  }
+
+                  update_comments();
+              </script>
+          <?php } } ?>
+          </section>
+      </main>
+      <?php include('includes/footer.php') ; ?>
+
+      <script src="../scripts/markdown.js" charset="utf-8"></script>
+      <script src="../scripts/blogVerifications.js" charset="utf-8"></script>
+  </body>
 </html>
--- blog.php
+++ blog.php
@@ -11,69 +11,73 @@
         <?php include('includes/header.php'); ?>
 
         <main role="main" class="container">
-            <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#blogPostModal">Publier un nouvel article</button>
-            <?php include('includes/blog/new_post.php'); ?>
+          <?php if (isset($_SESSION['userid'])) { ; ?>
+          <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#blogPostModal">Publier un nouvel article</button>
+          <?php include('includes/blog/new_post.php'); ?>
+        <?php } else { ?>
+          <div class="alert alert-info float-right">Vous devez être connecté pour publier un article.</div>
+        <?php } ?>
 
-            <h2>Blog</h2>
-            <hr>
-            <?php include('includes/blog/page_nav.php') ; ?>
+          <h2>Blog</h2>
+          <hr>
+          <?php include('includes/blog/page_nav.php') ; ?>
 
-            <?php
-            $query = $pdo->prepare('SELECT COUNT(id_m) FROM message WHERE category = ?') ;
-            $query->execute([$page_name]) ;
-            $elements = $query->fetch()[0] ;
+          <?php
+          $query = $pdo->prepare('SELECT COUNT(id_m) FROM message WHERE category = ?') ;
+          $query->execute([$page_name]) ;
+          $elements = $query->fetch()[0] ;
 
-            $query = $pdo->prepare('SELECT id_m, title, content, date_published, username, icon, file_name FROM message
-              LEFT JOIN users ON id_u = author
-              LEFT JOIN language ON default_language = lang
-              LEFT JOIN file ON message = id_m
-              WHERE category = ?
-              ORDER BY date_published, latest_c ASC') ;
-            $query->execute([$page_name]) ;
-            $messages = $query->fetchAll() ;
+          $query = $pdo->prepare('SELECT id_m, title, content, date_published, username, icon, file_name FROM message
+            LEFT JOIN users ON id_u = author
+            LEFT JOIN language ON default_language = lang
+            LEFT JOIN file ON message = id_m
+            WHERE category = ?
+            ORDER BY date_published, latest_c ASC') ;
+          $query->execute([$page_name]) ;
+          $messages = $query->fetchAll() ;
 
-            $offset = $page_limit * ($page - 1) ;
-            for ($i = $offset ; $i < $offset + $page_limit && $i < $elements ; ++$i) {
-                $article = $messages[$i] ;
-                $query = $pdo->prepare('SELECT COUNT(user) FROM vote_message WHERE message = ?') ;
-                $query->execute([$article['id_m']]) ;
-                $mark = $query->fetch()[0] ;
-            ?>
-            <section class="card mb-3" style="max-width: width;">
-                <div class="row no-gutters">
-                    <aside class="col-md-4">
-                        <img src="
-                        <?php if(!empty($article['file_name'])) echo 'images/' . $article['file_name'] ;
-                        else echo 'https://www.meetech.ovh/images/logov4.svg' ; ?>" alt="Image article" style="max-width: 200px; max-height: 200px;">
-                    </aside>
-                    <article class="col-md-8">
-                        <div class="card-body">
-                            <div class="float-right">
-                                <span class="badge badge-pill badge-success"><?= $mark ; ?></span>
-                                <span class="badge badge-pill badge-danger">!</span>
-                            </div>
-                            <h5 class="card-title">
-                              <span><?= $article['icon'] ?></span>
-                              <a href="/article.php?post=<?= $article['id_m'] ; ?>"><?= $article['title'] ?></a>
-                            </h5>
-                            <p class="card-text">
-                              <?= substr($article['content'], 0, 270) .'…' ; ?>
-                              <a href="/article.php?post=<?= $article['id_m'] ; ?>"> » Continue reading</a>
-                            </p>
-                            <p class="card-text">
-                                <small class="text-muted">Published on <?= $article['date_published'] ?> by <a href="/user/?id=<?= $article['id_m'] ; ?>"><?= $article['username'] ; ?></a>
-                                </small>
-                            </p>
-                        </div>
-                    </article>
-                </div>
-            </section>
-            <?php
-            } ;
-            include('includes/blog/page_nav.php') ;
-            ?>
-        </main>
+          $offset = $page_limit * ($page - 1) ;
+          for ($i = $offset ; $i < $offset + $page_limit && $i < $elements ; ++$i) {
+              $article = $messages[$i] ;
+              $query = $pdo->prepare('SELECT COUNT(user) FROM vote_message WHERE message = ?') ;
+              $query->execute([$article['id_m']]) ;
+              $mark = $query->fetch()[0] ;
+          ?>
+          <section class="card mb-3" style="max-width: width;">
+              <div class="row no-gutters">
+                  <aside class="col-md-4">
+                      <img src="
+                      <?php if(!empty($article['file_name'])) echo 'images/' . $article['file_name'] ;
+                      else echo 'https://www.meetech.ovh/images/logov4.svg' ; ?>" alt="Image article" style="max-width: 200px; max-height: 200px;">
+                  </aside>
+                  <article class="col-md-8">
+                      <div class="card-body">
+                          <div class="float-right">
+                              <span class="badge badge-pill badge-success"><?= $mark ; ?></span>
+                              <span class="badge badge-pill badge-danger">!</span>
+                          </div>
+                          <h5 class="card-title">
+                            <span><?= $article['icon'] ?></span>
+                            <a href="/article.php?post=<?= $article['id_m'] ; ?>"><?= $article['title'] ?></a>
+                          </h5>
+                          <p class="card-text">
+                            <?= substr($article['content'], 0, 270) .'…' ; ?>
+                            <a href="/article.php?post=<?= $article['id_m'] ; ?>"> » Continue reading</a>
+                          </p>
+                          <p class="card-text">
+                              <small class="text-muted">Published on <?= $article['date_published'] ?> by <a href="/user/?id=<?= $article['id_m'] ; ?>"><?= $article['username'] ; ?></a>
+                              </small>
+                          </p>
+                      </div>
+                  </article>
+              </div>
+          </section>
+          <?php
+          } ;
+          include('includes/blog/page_nav.php') ;
+          ?>
+      </main>
 
-        <?php include('includes/footer.php'); ?>
+      <?php include('includes/footer.php'); ?>
     </body>
 </html>
