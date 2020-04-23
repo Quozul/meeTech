10083f0a9a347e7f610778f1a19b3e99a457c37d
RevanSpec <desbrueresa1@gmail.com>
2020-04-23 22:35:08 +0200

    Connexion auto post token

--- verification.php
+++ verification.php
@@ -3,18 +3,34 @@
 <?php include('includes/head.php'); ?>
 
 <body class="d-flex vh-100 flex-column justify-content-between">
-    <?php include('includes/header.php');
-    $sth = $pdo->prepare('UPDATE users SET verified = TRUE WHERE token =?');
-    $sth->execute([$_GET['token']]);
-    ?>
+    <?php include('includes/header.php'); ?>
     <main role="main" class="container">
         <h1>Vérification</h1>
         <div class="jumbotron">
             <p class="text-justify">
-                Votre compte a été validé avec succés cliquez <a href="/">ici</a> pour revenir à l'acceuil de meetech
+                <?php
+                if (isset($_GET['token'])) {
+                    $sth = $pdo->prepare('UPDATE users SET verified = TRUE WHERE token =?');
+                    $token = htmlspecialchars(trim($_GET['token']));
+                    $sth->execute([$token]);
+
+                    $query = $pdo->prepare('SELECT id_u, verified FROM users WHERE token =?');
+                    $query->execute([$token]);
+                    $res = $sth->fetch();
+
+                    if (!$res['verified'])
+                        echo "Le compte n'est pas validé";
+                    else {
+                        $_SESSION['userid'] = $res[0];
+                ?>
+                        Votre compte a été validé avec succés cliquez <a href="/">ici</a> pour revenir à l'acceuil de meetech
+                <?php
+                    }
+                } else {
+                    echo "Le token n'est pas défini !";
+                } ?>
             </p>
         </div>
-
     </main>
     <?php include('includes/footer.php'); ?>
 </body>
