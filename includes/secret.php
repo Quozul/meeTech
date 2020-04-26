3de29b9c1627963f78eee9235baafdcd30f13d2b
Quozul <erwan.legloannec@dartybox.com>
2020-04-26 12:39:27 +0200

    Not very secret area
    Where's WebASM ?

---
+++ includes/secret.php
@@ -0,0 +1,60 @@
+<?php
+$languages = ['A+', 'A++', 'A# .NET', 'A# (Axiom)', 'A-0 System', 'ABAL', 'ABAL++', 'ABAP', 'ABC', 'ABCL/1', 'ABCL/c+', 'ABCL/R', 'ABCL/R2', 'Abel', 'ABSET', 'ABSYS', 'ALI', 'Abundance', 'ACC', 'Accent', 'ActForex', 'Ace DASL', 'ACT-III', 'Ada', 'Adenine', 'Afnix', 'Agora', 'AIS Balise', 'Aikido', 'Alef', 'Algebraic Logic Functional programming language', 'Algol 60', 'Algol 68', 'Algol W', 'Alice', 'Ambi', 'Amiga E', 'AML', 'AMOS', 'AMPLE', 'Anubis', 'APDL', 'APL', 'AppleScript', 'Arc', 'Ariberion', 'Arobase (langage)', 'Assembleur', 'ASP.NET', 'ATS', 'AutoHotkey', 'AutoIt', 'Averest', 'awk', 'axe parser', 'Axum', 'B', 'BASIC', 'BASICA', 'BlitzBasic', 'DarkBasic', 'FreeBasic', 'GW-Basic', 'Microsoft Small Basic', 'PowerBasic', 'PureBasic', 'QuickBasic', 'SmallBasic', 'TI-Basic', 'True Basic', 'Turbo Basic', 'Visual Basic', 'wxBasic', 'Bash (Bourne-Again shell)', 'Batch file (fichier bat)', 'bc', 'BCPL', 'BeanShell', 'Befunge', 'Bennu', 'Bertrand', 'BETA', 'Bigwig', 'Bistro', 'BitC', 'BLISS', 'BlitzBasic', 'Blueprint Visual Scripting', 'Blue', 'Bon', 'Boo', 'Boomerang', 'Bosque', 'Bourne shell (sh)', 'BPEL', 'Brainfuck', 'BUGSYS', 'BuildProfessional', 'C', 'C--', 'C++', 'C#', 'C/AL', 'Caché ObjectScript', 'Cameleon', 'Caml', 'Cat', 'Cayenne', 'Cecil', 'Cel', 'Cesil', 'Ceylon', 'CFML', 'Cg', 'Ch interpreter', 'Chapel', 'CHAIN', 'Charity', 'Chef', 'CHILL', 'CHIP-8', 'chomski', 'CHR', 'Chrome', 'ChucK', 'CICODE', 'CIL', 'Cilk', 'CL', 'Claire', 'Clarion', 'Clean', 'Clipper', 'CLIST', 'Clojure', 'CLU', 'CMS-2', 'COBOL', 'CobolScript', 'Cobra', 'CODE', 'CoffeeScript', 'Cola', 'ColdC', 'ColdFusion', 'COMAL', 'Common Lisp', 'COMPASS', 'Component Pascal', 'COMIT', 'Converge', 'Cool', 'Coq', 'Coral 66', 'Corn', 'CorVision', 'Cosmos-f', 'COWSEL', 'CPL', 'Crystal', 'C shell (csh)', 'CSP', 'Csound', 'CSS', 'CSS 3', 'Cubescript', 'Curl', 'Curry', 'Cyclone', 'D', 'DaKeoZ', 'Dao', 'DASL - Distributed Application Specification Language', 'DASL - Datapoint\'s Advanced Systems Language', 'DarkBASIC', 'Dart', 'DataFlex', 'Datalog', 'dBase', 'dc', 'DCL', 'Deesel', 'G', 'Delphi', 'Dialect', 'DinkC', 'Dialog Manager', 'DIBOL', 'Down', 'Duke C', 'Dylan', 'Dynace', 'E', 'Ease', 'EASY', 'Easytrieve Classic', 'Easytrieve Plus', 'eC (Ecere C)', 'ECMAScript', 'eDeveloper', 'Edinburgh IMP', 'EGL', 'Eiffel', 'Einstein', 'Elan', 'elastiC', 'Elf', 'Emacs Lisp', 'Englesi', 'Epigram', 'Erlang', 'Escapade', 'Escher', 'Esterel', 'Etoys', 'Euclid', 'Euler', 'Euphoria', 'CMS EXEC', 'EXEC 2', 'Extends', 'F', 'F#', 'Factor', 'Falcon', 'False', 'Fan', 'Fancy', 'Fantom', 'FAUST', 'Felix', 'Ferite', 'FFP', 'Fjölnir', 'FL', 'Flavors', 'FLOW-MATIC', 'Fly', 'FOCAL', 'FOCUS', 'FOIL', 'FORMAC', '@Formula', 'Forth', 'Fortran', 'Fortress', 'FoxPro 2', 'FP', 'Franz Lisp', 'F-Script', 'FSProg', 'Funnel', 'Fuxi', 'Gambas', 'GAMS', 'GAP (RGP)', 'GAP (logiciel de calcul formel)', 'G-code', 'GDL', 'Gibiane', 'GFA BASIC', 'GJ', 'GLSL', 'GM', 'GML (Game Maker Language)', 'Go', 'Go!', 'GOAL', 'GDScript', 'Gödel', 'Godiva', 'Goo', 'GOTO++', 'GOTRAN', 'GPSS', 'GraphTalk', 'GRASS', 'Grammer', 'Green', 'Groovy', 'Hack', 'HAL/S', 'Haskell', 'Haxe', 'High Level Assembly', 'HLSL', 'Hop', 'Hope', 'Hugo', 'Hume', 'HyperTalk', 'IBM Basic Assembly Language (BAL)', 'IBM Informix-4GL', 'IBM RPG', 'ICI', 'Icon', 'Id', 'IDL (Interactive Data Language)', 'IL', 'IMP', 'Inform', 'INTERCAL', 'Io', 'Ioke', 'IPL', 'IPTSCRAE', 'ISWIM', 'J', 'J#', 'JADE', 'Jako', 'Jackson Structured Programming', 'JAL', 'Janus', 'JASS', 'Java', 'JavaScript', 'JCL (Job Control Langage)', 'JEAN', 'Join Java', 'JOSS', 'Joule', 'JOVIAL', 'Joy', 'JSP', 'Julia', 'Kafe', 'Kaleidoscope', 'Karel', 'Karel++', 'Kaya', 'KEE', 'Kiev', 'KIF', 'Kite', 'Kogut', 'Korn shell (ksh)', 'Kotlin', 'KRC', 'KRYPTON', 'KUKA', 'L', 'L# .NET', 'LabVIEW', 'Ladder (LD)', 'Lagoona', 'LANSA', 'LARP', 'Lasso', 'Lava', 'Leadwerks Script', 'Leda', 'leekwars', 'Legoscript', 'Lexico', 'Limbo', 'Limnor', 'LINC', 'Lingo', 'Linoleum', 'Linotte', 'LIS', 'LISA', 'Lisaac', 'Lisp', 'Lithe', 'Little b', 'Logo', 'LOLCODE', 'LPC', 'LSE', 'LSL', 'Lua', 'Lucid', 'Lush', 'Lustre', 'LYaPAS', 'Lynx', 'LZX', 'M', 'M4', 'Maxima', 'MAD', 'Magik', 'Magma', 'Malbolge', 'MapBasic', 'Maple', 'MAPPER', 'MARK-IV', 'Mary', 'Microsoft Macro Assembler', 'Micro Lua DS', 'Mathematica', 'MATLAB', 'MaxScript (Autodesk 3ds Max)', 'Maya (MEL)', 'MDL', 'Mercury', 'Mesa', 'MetaL', 'MicroAlg', 'Microcode', 'MicroScript', 'MIIS', 'MillScript', 'MIMIC', 'min', 'MisterDev', 'Mindscript', 'Miranda', 'mIRC', 'Miva Script', 'ML', 'Moby', 'Model 204', 'Modula', 'Modula-2', 'Modula-3', 'Mondrian', 'MOO', 'Mortran', 'Moto', 'Mouse', 'MPD', 'MPP', 'MSIL (CIL)', 'MSL', 'MSX BASIC', 'MUMPS', 'Mythryl', 'N', 'Napier88', 'Natural', 'NBC', 'NEAT chipset', 'Neko', 'Nemerle', 'Neonitisme', 'NESL', 'NestJs', 'Net.Data', 'NetLogo', 'NewLISP', 'NEWP', 'NewtonScript', 'NGL', 'Nial', 'Nice', 'Nickle', 'Nil', 'Nit', 'Node.js', 'NPL', 'NQC', 'Nu', 'NXC', 'NXT-G', 'Nyquist', 'o:XML', 'Oak', 'Oberon', 'Object Lisp', 'ObjectLOGO', 'Objective-C', 'Objective-J', 'Obliq', 'Obol', 'OCaml', 'occam', 'occam-p', 'OmniMark', 'ooc', 'Opa', 'Opal', 'OpenEdge ABL', 'OPL', 'OPS5', 'Ork', 'ORCA/Modula-2', 'Orwell', 'Oxygene', 'Oz', 'Panoramic', 'PARI/GP', 'Pascal', 'Pascal Objet', 'Pawn', 'PCASTL', 'PCF', 'PEARL', 'Perl', 'Pharo', 'PHP', 'Phrogram', 'Pico', 'Pict', 'Piet', 'Pike', 'PIKT', 'PILOT', 'Pizza', 'Pi-calcul', 'PL-11', 'PL/0', 'PL/B', 'PL/C', 'PL/I', 'PL/M', 'PL/P', 'PL/SQL', 'PL360', 'PLANC', 'Plankalkül', 'PLEX', 'PLEXIL', 'Pliant', 'POP-11', 'Poplog', 'Portal', 'PostScript', 'PortablE', 'PowerBuilder', 'Powerhouse', 'PowerShell', 'PPL', 'Processing', 'Prograph', 'Progress 4GL', 'PROIV', 'Prolog', 'Promela', 'PROTEL', 'Proteus', 'ProvideX', 'Pure', 'PureBasic', 'Python', 'Q', 'Q#', 'Qi', 'QtScript', 'QBasic', 'QML', 'QuakeC', 'QPL', 'R', 'R++', 'Rapira', 'Ratfiv', 'Ratfor', 'RBScript', 'rc', 'Rebol', 'Red', 'Redcode', 'REFAL', 'Reia', 'Revolution', 'REXX', 'Rlab', 'ROOP', 'RPG', 'RPL', 'RSL', 'RTL/2', 'Ruby', 'Rust', 'S', 'S2', 'S3', 'S-Lang', 'SA-C', 'SAC', 'Safe C', 'SAIL', 'SALSA', 'SAM76', 'SAS', 'SASL', 'Sather', 'Sawzall', 'SBL', 'Scala', 'Scheme', 'Scilab', 'Scol', 'Scratch', 'Script.NET', 'Sed', 'Seed7', 'Self', 'SETL', 'Shakespeare Programming Language', 'Shift Script', 'Signal', 'SiMPLE', 'SIMPOL', 'SIMSCRIPT', 'Simula', 'SISAL', 'Slate', 'SLIP', 'SMALL', 'Smalltalk', 'SML', 'SNOBOL', 'SPITBOL', 'Snap!', 'Snow', 'Snowball', 'SNUSP', 'SOL', 'Span', 'SPARK', 'Spice', 'SPIN', 'SP/k', 'SPS', 'Squeak', 'Squirrel', 'SR', 'SSL', 'STOS BASIC', 'Strand', 'Subtext', 'Suneido', 'SuperCollider', 'Swift', 'SYMPL', 'SyncCharts', 'SystemVerilog', 'Sourcepawn', 'T', 'TACL', 'TACPOL', 'TADS', 'TAL', 'Tcl', 'Tea', 'TECO', 'TELCOMP', 'CA-Telon', 'TenCORE', 'Terra', 'TEX', 'TeX', 'Ticol', 'TIE', 'Tiger', 'Thue', 'TI-Basic', 'TI-Extended Basic', 'Timber', 'Tom', 'TOM', 'Topspeed', 'TPU', 'Trac', 'Transact-SQL', 'TTCN', 'Turing', 'TUTOR', 'TXL', 'Turbo Pascal', 'TypeScript', 'Ubercode', 'Unicon', 'Uniface', 'uniPaaS', 'UNITY', 'unityscript', 'Unix shell', 'Unlambda', 'UnrealScript', 'USE', 'V', 'Vala', 'VBA', 'VBScript', 'Verilog', 'VHDL', 'Visual Adélia', 'Visual Basic (VB6)', 'Visual Basic .NET', 'Visual DataFlex', 'Visual DialogScript', 'Visual FoxPro', 'Visual Objects', 'Vvvv', 'Water', 'WATFIV, WATFOR', 'WebQL', 'Whitespace', 'Winbatch', 'WLangage (WinDev)', 'Windows PowerShell', 'X#', 'X++', 'X10', 'XBL', 'xHarbour', 'XL', 'XOTcl', 'XPL', 'XPL0', 'XQuery', 'XSLT', 'Y', 'YaBasic', 'YAL', 'Yoix', 'Yorick', 'Z notation', 'Zeno', 'Zonnon', 'ZOPL', 'ZPL', 'ZZT-oop'];
+
+switch ($_GET['lang']) {
+    case 'C':
+        echo 'Le C est un langage apprécié de M. Sananes.';
+        echo '<br>Le langage JavaScript semble supérieur !';
+        break;
+    case 'JavaScript':
+        echo 'Le JavaScript est un langage apprécié de M. Briatte, il est l\'[object Object] de grandes tensions entre M. Sananes et ce dernier.';
+        echo '<br>Le langage C semble supérieur !';
+        break;
+    case 'Java':
+        echo 'Il n\'est pas très apprécié par les développeurs de JavaScript lorsqu\'il est utilisé pour le qualifier.';
+        echo '<br>Le langage C# semble supérieur !';
+        break;
+    case 'C++':
+        echo 'Pour ceux qui préfère avoir accès à des simplifications du C.';
+        echo '<br>Le langage C€ semble supérieur !';
+        break;
+    case 'C#':
+        echo 'Aussi appelé "Microsoft Java".';
+        echo '<br>Le langage Java semble supérieur !';
+        break;
+    case 'Crystal':
+        echo 'As Fast As C, As Slick As Ruby.';
+        echo '<br>Le langage Ruby semble supérieur !';
+        break;
+    case 'Haxe':
+        echo 'Pour ceux qui ne veulent pas se cacher derrière un langage, utiliser celui-ci. Il doit être traduit dans un autre langage avant d\'être compilé.';
+        echo '<br>Le langage ActionScript semble supérieur !';
+        break;
+    case 'Ruby':
+        echo 'Le C du futur.';
+        echo '<br>Le langage Perl semble supérieur !';
+        break;
+    case 'Lua':
+        echo 'Langage très littéral dans sa syntaxe.';
+        echo '<br>Le langage Python semble supérieur !';
+        break;
+    case 'Hugo':
+        echo 'C\'est le terroriste de notre classe.';
+        echo '<br>Le langage Liquid semble supérieur !';
+        break;
+    case 'SQL':
+        echo 'Il permet d\'interragir avec de grandes quantités de données.';
+        echo '<br>Le langage NoSQL semble supérieur !';
+        break;
+    case 'Node.js':
+        echo 'Le Node.js utilise le JavaScript comme base avec quelques variations.';
+        echo '<br>Le langage PHP semble supérieur !';
+        break;
+
+    default:
+        echo $_GET['lang'];
+        srand(array_search($_GET['lang'], $languages));
+
+        echo '<br>Le langage ' . $languages[rand(0, count($languages) - 1)] . ' semble supérieur !';
+        break;
+}
